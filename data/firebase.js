ef1a983b68e4ae31dd7ef142ad38de07eed73545
mpoweredo <mpow3r1337@gmail.com>
2022-07-22 15:26:56 +0200

    Passed credentials into file

--- data/firebase.js
+++ data/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.apiKey,
-  authDomain: process.env.authDomain,
-  projectId: process.env.projectId,
-  storageBucket: process.env.storageBucket,
-  messagingSenderId: process.env.messagingSenderId,
-  appId: process.env.appId
+  apiKey: "AIzaSyA2Q_9J-tKZK1FjPEui1SneX38mVZEr7Tk",
+  authDomain: "chat-app-vercel.firebaseapp.com",
+  projectId: "chat-app-vercel",
+  storageBucket: "chat-app-vercel.appspot.com",
+  messagingSenderId: "1069542585360",
+  appId: "1:1069542585360:web:fb3fef6af9bfd56f68c695"
 };
 
 export const app = initializeApp(firebaseConfig, 'dasd')
